c66c0158a22942a2ceddce0ecb7885b7f8613ef4
HuyHQ <huyhq@deha-soft.com>
2022-09-04 08:53:36 +0700

    update

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+
+
+    /*
+        1. nft to point to an address
+        2. keep track o the token ids
+        3 keep track of token owner addresses to token ids
+        4 keep track of how many token owner address 
+        5 create an event that emits a tanfer log -contracst address
+    */
+
+
+contract ERC721 {
+
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 indexed tokenId);
+
+    // mapping token id to owner
+    mapping (uint256 => address) private _tokenOwner;
+
+    // mapping owner to number token
+
+    mapping(address => uint256) _ownerToTokenCount;
+
+    function _exists(uint256 tokenId) internal view returns(bool){
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        // required address not zero
+        require(to != address(0), 'ERC721 to mint to zero adress');
+        // check token exists
+        require(_exists(tokenId), 'ERC721: toekn already to mint');
+        // we adding a new address with token id
+        _tokenOwner[tokenId] = to;
+        // keep track each addres that mint 
+        _ownerToTokenCount[to] += 1;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+}
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,6 +4,23 @@ pragma solidity ^0.8.16;
 import "./ERC721Connector.sol";
 
 contract KryptoBird is ERC721Connector {
+
+  // array to store our nft
+  string[] public kryptoBirdz;
+
+  mapping(string => bool) _kryptoBirdExists;
+
+  function mint(string memory _krytobird) public {
+      require(!_kryptoBirdExists[_krytobird], 'Error: krytobird already exists');
+
+
+      kryptoBirdz.push(_krytobird);
+
+      uint _id = kryptoBirdz.length - 1;
+      _mint(msg.sender, _id);
+      _kryptoBirdExists[_krytobird] = true;
+  }
+
   constructor() ERC721Connector('KryptoBird', 'KBIRD')
   {}
 }
